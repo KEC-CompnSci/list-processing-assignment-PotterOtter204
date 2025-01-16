18f392dfd8951073faaac0ebdbb5adf1c2ceb3b3
PotterOtter204 <76660894+PotterOtter204@users.noreply.github.com>
2025-01-16 20:46:05 +0000

    test passed

--- src/assignment.py
+++ src/assignment.py
@@ -1,4 +1,4 @@
-# src/assignment.py
+import math
 """
 List Processing Assignment
 
@@ -20,7 +20,17 @@ def find_peaks(numbers):
     Example:
         find_peaks([1, 3, 2, 3, 5, 3]) -> [3, 5]
     """
-    pass
+    length = len(numbers)
+    peaks = []
+    for i in range(length):
+        if i > 0 and i < length-1:
+            val = numbers[i]
+            if val > numbers[i-1] and val > numbers[i+1]:
+                peaks.append(val)
+
+    return peaks
+
+
 
 def running_average(numbers, window_size):
     """
@@ -36,7 +46,19 @@ def running_average(numbers, window_size):
     Example:
         running_average([1, 2, 3, 4, 5], 3) -> [2.0, 3.0, 4.0]
     """
-    pass
+    length = len(numbers)
+    i = 0
+    averages = []
+
+    if window_size > length:
+        raise ValueError("window size too big")
+    while i <= length -window_size:
+        slice = numbers[i: i + window_size]
+        answer = sum(slice)/window_size
+        answer = round(answer, 2)
+        averages.append(answer)
+        i += 1
+    return averages
 
 def find_duplicates(numbers):
     """
@@ -52,4 +74,14 @@ def find_duplicates(numbers):
     Example:
         find_duplicates([1, 2, 2, 3, 3, 3, 4]) -> [2, 3]
     """
-    pass
+    numDict = {}
+    duplicates = []
+    for num in numbers:
+        if num not in numDict:
+            numDict[num] = 1
+        else:
+            
+            if numDict[num] ==1:
+                duplicates.append(num)
+            numDict[num] +=1
+    return duplicates
